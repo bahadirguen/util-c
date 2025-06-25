20ea75b859279d08755fe8edda8c181a8bb423d2
Bahadir Guen <bahadirguen@gmail.com>
2025-06-25 22:44:59 +0200

    Use size_t for sizes and indexes

--- array.c
+++ array.c
@@ -1,24 +1,25 @@
 #ifndef _BG_ARRAY_C
 #define _BG_ARRAY_C
 
+#include <stddef.h>
 #include <stdint.h>
 
 #include "arena.c"
 
 typedef struct U32Array {
   uint32_t* head;
-  uint32_t size;
+  size_t size;
 } U32Array;
 
-U32Array* allocU32Array(SimpleArena* arena, uint32_t size) {
-  const uint32_t struct_size = sizeof(U32Array);
-  const uint32_t total_size = struct_size + size;
+U32Array* allocU32Array(SimpleArena* arena, const size_t size) {
+  const size_t struct_size = sizeof(U32Array);
+  const size_t total_size = struct_size + size;
   void* buf = allocArena(arena, total_size);
   if (buf == NULL) {
     return NULL;
   }
   U32Array* arr = (U32Array*)buf;
-  arr->head = (uint32_t*)(buf) + struct_size;
+  arr->head = (uint32_t*)((size_t*)(buf) + struct_size);
   arr->size = size;
   return arr;
 }
--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,26 +8,23 @@
 
 int main(int argc, char** argv) {
   printf("Program %s: called with %d arguments\n", argv[0], argc);
-  uint32_t arena_size = (1 << 10);
+
+  const size_t arena_size = (1 << 10);
   SimpleArena* arena = initArena(arena_size);
   printf("Arena buf: %p\n", arena->buf);
-  printf("Arena idx: %d\n", arena->idx);
-  uint32_t alloc_size = (1 << 8);
-  for (uint32_t i = 0; i < 5; i++) {
-    // void* buf = allocArena(arena, alloc_size);
+  printf("Arena idx: %zu\n", arena->idx);
+  const size_t alloc_size = (1 << 8);
+  for (size_t i = 0; i < 5; i++) {
     U32Array* arr = allocU32Array(arena, alloc_size);
-    // if (buf == NULL) {
     if (arr == NULL) {
-      fprintf(stderr, "Allocation attempt %d failed, exiting\n", i + 1);
+      fprintf(stderr, "Allocation attempt %zu failed, exiting\n", i + 1);
       break;
     }
-    printf("Arena idx: %d\n", arena->idx);
+    printf("Arena idx: %zu\n", arena->idx);
 
-    // uint32_t* arr = (uint32_t*)buf;
-    // arr[alloc_size - 1] = 2 * (i + 1);
-    // printf("arr[%d]: %d\n", alloc_size - 1, arr[alloc_size - 1]);
-    arr->head[alloc_size - 1] = 2 * (i + 1);
-    printf("arr[%d]: %d\n", alloc_size - 1, arr->head[alloc_size - 1]);
+    const size_t last_idx = alloc_size - 1;
+    arr->head[last_idx] = 2 * (i + 1);
+    printf("arr[%zu]: %d\n", last_idx, arr->head[last_idx]);
   }
   freeArena(arena);
   return EXIT_SUCCESS;
